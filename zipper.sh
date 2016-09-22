ca442a4f81983e67ea1d94104eada2779685ffa6
Gérald Plusquellec <gplusquellec@gmail.com>
2016-09-22 08:59:21 +0200

    first commit

--- zipper.sh
+++ zipper.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 wget https://github.com/rueduphp/skeleton/archive/master.zip
 unzip master.zip -d working
-cd working/laravel-master
+cd working/skeleton-master
 composer install
-zip -ry ../../laravel-craft.zip .
+zip -ry ../../skeleton-craft.zip .
 cd ../..
-mv laravel-craft.zip public/laravel-craft.zip
+mv skeleton-craft.zip public/skeleton-craft.zip
 rm -rf working
 rm master.zip
